46c755aa7dd3e1fd0a59ec9ef63d4f705fa2faeb
NGUYỄN HUY THÀNH <21127430@student.hcmus.edu.vn>
2022-07-04 10:33:52 +0700

    Add project files.

---
+++ Client/framework.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "targetver.h"
+#include <stdio.h>
+#include <tchar.h>
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include <afx.h>
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Control
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>                     // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <algorithm>
+#include "afxsock.h"
+#include <Windows.h> 
+#include <fstream>
+using namespace std;
+
+enum statusCursor { UP, DOWN, LEFT, RIGHT, enter, BACK };
+
+class consoleGraphic
+{
+public:
+	int heigth = 700, width = 1320;
+	consoleGraphic();
+	void resizeConsole();
+	void gotoxy(int x, int y);
+	void FixConsoleWindow();
+	void ShowCur(bool CursorVisibility);
+	void drawBox(int x, int y, int h, int w);
+};
+
+struct user {
+	string username, strpass;
+};
+
+class client
+{
+public:
+	client();
+	// Register
+	bool registerValid(user clientA);
+	// Log in
+	bool loginValid(user clientA);
+};
---
+++ Client/pch.cpp
@@ -0,0 +1,108 @@
+// pch.cpp: source file corresponding to the pre-compiled header
+
+#include "pch.h"
+#include "framework.h"
+// When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
+
+consoleGraphic::consoleGraphic() {
+	resizeConsole();
+	FixConsoleWindow();
+}
+
+void consoleGraphic::FixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+}
+
+void consoleGraphic::resizeConsole() {
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, this->width, this->heigth, TRUE);
+}
+
+void consoleGraphic::gotoxy(int x, int y) {
+	HANDLE hConsoleOutput;
+	COORD Cursor_an_Pos = { x , y };
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
+}
+
+void consoleGraphic::ShowCur(bool CursorVisibility) {
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
+	SetConsoleCursorInfo(handle, &cursor);
+}
+
+client::client() {
+	consoleGraphic conSole;
+	user clientA;
+	do {
+		cout << "Username: "; conSole.gotoxy(30, 0); cout << "(Username is \'0\' to \'9\' and \'a\' to \'z\')\n";
+		cout << "Password: ";
+		conSole.gotoxy(10, 0);
+		cin >> clientA.username;
+		conSole.gotoxy(10, 1);
+		cin >> clientA.strpass;
+		if (registerValid(clientA) == 0) {
+			conSole.gotoxy(0, 3); cout << "Username or password is not valid. Please try another one.\n";
+			system("pause");
+			system("cls");
+		}
+	} while (registerValid(clientA) == 0);
+
+	fstream out;
+	out.open("Text.txt", ios::app);
+	out << clientA.username << ' ' << clientA.strpass << endl;
+	out.close();
+	/*for (int i = 0;; i++) {
+		cin >> strPassword[i];
+		conSole.gotoxy(9 + i, 1);
+		cout << "*";
+		if (strPassword[i] == '\n') {
+			strPassword[i] = '\0';
+			break;
+		}
+	}*/
+	/*if (loginValid(clientA))
+		cout << "valid!";
+	else
+		cout << "Username or Password doesn't correct.\n";*/
+}
+
+// phan nay cua server lam nham qua client :)
+//bool client::loginValid(string strA, string strB) {
+//	user s[3];
+//	s[0].username = "thanh";
+//	s[1].username = "phuoc";
+//	s[2].username = "thai";
+//	s[0].strpass = "thanh123";
+//	s[1].strpass = "thanh123";
+//	s[2].strpass = "thanh123";
+//	for (int i = 0; i < 3; i++) {
+//		if (strA == s[i].username) {
+//			for (int j = 0; j < strB.length(); j++) {
+//				if (s[i].strpass[j] != strB[j])
+//					return 0;
+//			}
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+
+bool client::loginValid(user clientA) {
+	return 1;
+}
+
+bool client::registerValid(user clientA) {
+	if (clientA.username.length() < 5 || clientA.strpass.length() < 3)
+		return 0;
+	for (int i = 0; i < clientA.username.length(); i++) {
+		if ((clientA.username[i] < '0' || clientA.username[i] > '9') && (clientA.username[i] < 'a' || clientA.username[i] > 'z'))
+			return 0;
+	}
+	return 1;
+}
